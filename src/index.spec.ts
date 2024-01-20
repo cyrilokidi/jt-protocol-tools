7c7f51babb545527838fb56b77ec96bb52467550
CyrilOkidi <okidicyril@gmail.com>
2024-01-20 12:45:39 +0300

    Refractor test config.

---
+++ src/index.spec.ts
@@ -0,0 +1,11 @@
+import { JT } from './index';
+
+describe("JT808", function () {
+    const data = "7e02000026123456789012007d02000000010000000200ba7f0e07e4f11c0028003c00001810151010100104000000640202007d01137e";
+
+    test("JT808 message", function () {
+        const result = "02000026123456789012007e000000010000000200ba7f0e07e4f11c0028003c00001810151010100104000000640202007d13";
+
+        expect(new JT.JT808(data).message.data).toBe(result);
+    });
+});
