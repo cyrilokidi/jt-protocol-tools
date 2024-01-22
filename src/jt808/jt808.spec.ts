cc228dd1e9457310cb9fab1f143b2a70dfe12ba7
CyrilOkidi <okidicyril@gmail.com>
2024-01-22 08:25:35 +0300

    Create jt808 term. reg. test case

--- src/jt808/jt808.spec.ts
+++ src/jt808/jt808.spec.ts
@@ -1,16 +1,11 @@
 import JT808 from "./jt808";
-import { JT808Message } from "./jt808.message";
 
 describe("JT808", function () {
-    const data = "7e0100002c202203100001000000000000000074730000004d5630340000000000000000000000000000000033313030303031014b43443737374b277e";
 
     test("Terminal registration", function () {
-        const result = "7e8100001120220310000100010000003833323032323033313030303000ba7e";
-        const { message, response } = new JT808(data);
+        const data = "7e0100002c202203100001000000000000000074730000004d5630340000000000000000000000000000000033313030303031014b43443737374b277e";
+        const result = "7e8100000f20220310000100010000003230323230333130303030319e7e";
 
-        expect(message).toBeInstanceOf(JT808Message);
-
-        console.log("response: ", response.response);
-
+        expect(new JT808(data).response.response).toBe(result);
     });
 });
